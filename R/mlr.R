66620ffcd819510ba34b86cf26f98181a667c30a
rxxwang <rxxwang@umich.edu>
2022-11-24 13:14:43 -0500

    add vignettes

--- R/mlr.R
+++ R/mlr.R
@@ -15,29 +15,26 @@
 #'mlr(mpg ~ cyl + wt + cyl * wt, mtcars)
 #'mlr(mpg ~ cyl + wt, mtcars, na.action = "na.exclude")
 #'
-#'@importFrom stats model.matrix
+#'@importFrom stats model.matrix na.exclude na.omit terms setNames
 #'
 #'@export
 #'
 mlr <- function(formula, data, na.action = "na.omit") {
   # handle NA
   if(na.action == "na.omit"){
-    data = na.omit(data)
+    data = stats::na.omit(data)
   }else if(na.action == "na.exclude"){
-    data = na.exclude(data)
+    data = stats::na.exclude(data)
   }
   # covert the formula to Y and X
   formula_y = all.vars(formula)[1]
-  formula_x = labels(terms(formula))
+  formula_x = labels(stats::terms(formula))
   # split the X with interactions
   covariates = unique(unlist(strsplit(formula_x,":")))
 
   # build a X matrix and a Y matrix
-  library(stats)
-  X = model.matrix(formula, data = data)
+  X = stats::model.matrix(formula, data = data)
   Y = as.matrix(data[formula_y])
-
-  #nothi
 
   # use Rcpp for the calculation of parameters
   # sourceCpp("Rcpp.cpp")
@@ -51,10 +48,10 @@ mlr <- function(formula, data, na.action = "na.omit") {
   fit_values = X %*% beta
 
   output_list = list(
-    "coefficients" = as.vector(beta),
-    "residuals" = as.vector(res),
+    "coefficients" = stats::setNames(as.vector(beta),rownames(beta)),
+    "residuals" = stats::setNames(as.vector(res),rownames(res)),
     "rank" = length(formula_x)+1,
-    "fitted.values" = as.vector(fit_values),
+    "fitted.values" = stats::setNames(as.vector(fit_values),rownames(fit_values)),
     "df.residual" = dim(data)[1] - length(formula_x) - 1,
     "call" = paste0("lm(formula = ", Reduce(paste, deparse(formula)), ")"),
     "terms" = formula,
@@ -69,7 +66,7 @@ mlr <- function(formula, data, na.action = "na.omit") {
     "\n",
     "Coefficients:","\n"
   )
-  print(t(beta))
+  print(stats::setNames(as.vector(beta),rownames(beta)))
 
   return(invisible(output_list))
 }
